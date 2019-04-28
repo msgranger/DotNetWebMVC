490e3e2302a91ffdfb7e8709981de56e1ee0fe0d
Arif EROL <arif.erol16@gmail.com>
2019-04-28 18:06:17 +0300

    .NET MVC sample project
    .NET MVC Entity framework CRUD sample project.

---
+++ CodeFirstSampleWeb/CodeFirstSampleWeb/BusinessLayer/IProductBS.cs
@@ -0,0 +1,27 @@
+﻿using EntitiesLayer.Concrete;
+using EntitiesLayer.ViewObjects;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IProductBS
+    {
+        List<Product> GetProducts();
+
+        Product Find(int? id);
+
+        List<Category> GetCategories();
+
+        List<Supplier> GetSuppliers();
+
+        int CreateProduct(Product product);
+
+        int UpdateProduct(Product product);
+
+        int DeleteProduct(int id);
+
+        List<ProductCategoryGroup> GetStats();
+
+        List<Product> GetProductsByName(string productName);
+    }
+}
